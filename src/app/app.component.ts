e6c03d54c3a2dc91176253d47176d2bf4b6aaa3d
Daniel <danielsalles@gmail.com>
2019-08-22 16:16:58 -0300

    Added all

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';''
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public amplify: AmplifyService) {
+    amplify.auth().currentAuthenticatedUser().then(console.log)
+  }
+
+  title = 'amplify-app';
+}
